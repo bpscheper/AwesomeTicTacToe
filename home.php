02aa6c8574e2d9a9284e9abd77199491b8e0922f
Brian Scheper <bpsc222@cs.uky.edu>
2013-11-06 22:54:23 -0500

    Modified home screen, rules screen, and added pictures

--- home.php
+++ home.php
@@ -1,4 +1,34 @@
 <?php
 
+session_start();
+
+echo '<html><body>';
+if (isset($_SESSION["user"])) {
+  echo '<div style="float:right">Hello, <a href="">';
+  $name = $_SESSION["user"];
+  echo $name . '.</a></div><br>';
+  echo '<div style="float:right"><a href="">Log out</a></div>';
+} else {
+  echo '<div style="float:right">Hello, <a href="login.php">';
+  echo 'Sign In</a></div><br>';
+  echo '<div style="float:right">Register Now</a></div>';
+}
+
+echo '<div><a href="home.php"><img src="Pictures/Name.jpg" 
+	alt="Awesome Tic Tac Toe"></a></div>';
+echo '<p><table><tr>';
+echo '<td><div><a href="rules.php"><img src="Pictures/Rules.jpg" alt="Rules">
+	</a></td><td><img src="Pictures/Picture1.jpg" alt="Example 1"></div>
+	</td></tr><tr>';
+echo '<td><div><a href="description.php"><img src="Pictures/Description.jpg" 
+	alt="Description"></a></div></td></tr><tr>';
+echo '<td><div><a href="game_play.php"><img src="Pictures/Play.jpg" 
+	alt="Start Playing!"></a></div></td><td><img 
+	src="Pictures/Picture2.jpg" alt="Example 2"></td></tr></table></p>';
+
+
+
+
+echo '</body></html>';
 
 ?>
---
+++ rules.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '<html><body>';
+echo '<img src="Pictures/Rules_title.jpg" alt="Rules of Awesome Tic Tac Toe">';
+echo '<p><font size="5" color="blue">1. This Tic Tac Toe game will be played 
+	on a 5x5 board. </font></p>';
+echo '<p><font size="5" color="blue">2. The player who created the game will 
+	start the game with the first move. </font></p>';
+echo '<p><font size="5" color="blue">3. Each player will take turns placing 
+	pieces on the board. </font></p>';
+echo '<p><font size="5" color="blue">4. Players have 30 seconds to place a
+	 piece or a square will be randomly selected for them. </font></p>';
+echo '<p><font size="5" color="blue">5. The longest string of pieces in a row, 
+	column, or diagonal will be the player score. (*Note: a string is 
+	considered to be more than one piece) </font></p>';
+echo '<p><font size="5" color="blue">6. The player with the longest 
+	string wins the game. </font></p>';
+
+echo '<a href="home.php"><img src="Pictures/Go_Home.jpg" alt="Go Home"></a>';
+echo '<a href="game_play.php"><img src="Pictures/start_playing.jpg" 
+	alt="Play"></a>';
+echo '</body></html>';
+
+?>
